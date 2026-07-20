d75b37366b446446f3089323a449d78e7d6dc39e
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-07-21 00:36:56 +0300

    role skill uploader

--- src/tasks/wot-img-loader/loaders/comp7.ts
+++ src/tasks/wot-img-loader/loaders/comp7.ts
@@ -2,7 +2,6 @@ import { GameVersion } from '@/tasks/wot-src-loader/utils'
 import { S3Client } from '@aws-sdk/client-s3'
 import { Ctx, filenameAndExtension } from '../utils'
 import { Glob } from 'bun'
-import { clickhouse } from '@/db'
 import { uploader } from '@/utils/assetsUploader'
 import sharp from 'sharp'
 
@@ -58,6 +57,20 @@ async function process(path: string, size: string, season: string, upload: Retur
   }
 }
 
+async function processRoleSkills(path: string, season: string, upload: ReturnType<typeof uploader>) {
+  const files = [...new Glob(path).scanSync()]
+  for (const filePath of files) {
+    const fileContent = await Bun.file(filePath).bytes()
+    const { nameWithoutExt: name, ext } = filenameAndExtension(filePath)
+
+    const webpBuffer = await sharp(fileContent).webp({ quality: 85, alphaQuality: 80 }).toBuffer()
+    await upload(`comp7/skills/${season}/${name}.png`, fileContent)
+    await upload(`comp7/skills/${season}/${name}.webp`, webpBuffer)
+    await upload(`comp7/ranks/latest/${name}.png`, fileContent)
+    await upload(`comp7/ranks/latest/${name}.webp`, webpBuffer)
+  }
+}
+
 let lastMtUploadKey = ''
 let lastWotUploadKey = ''
 export async function load(root: string, game: 'mt' | 'wot', version: string, bucket: S3Client, hasChanges: boolean, ctx: Ctx<Context>) {
@@ -80,6 +93,7 @@ export async function load(root: string, game: 'mt' | 'wot', version: string, bu
     await process(`${root}/gui/maps/icons/comp7/ranks/420/*.png`, 'large', context.season, upload)
     await process(`${root}/gui/maps/icons/comp7/ranks/150/*.png`, 'medium', context.season, upload)
     await process(`${root}/gui/maps/icons/comp7/ranks/84/*.png`, 'small', context.season, upload)
+    await processRoleSkills(`${root}/gui/maps/icons/roleSkills/180x180/*.png`, context.season, upload)
     lastMtUploadKey = uploadKey
 
   } else {
@@ -107,27 +121,11 @@ export async function load(root: string, game: 'mt' | 'wot', version: string, bu
       await process(`${root}/gui/maps/icons/comp7/ranks/${season}/420/*.png`, 'large', seasonName, upload)
       await process(`${root}/gui/maps/icons/comp7/ranks/${season}/150/*.png`, 'medium', seasonName, upload)
       await process(`${root}/gui/maps/icons/comp7/ranks/${season}/84/*.png`, 'small', seasonName, upload)
+      await processRoleSkills(`${root}/gui/maps/icons/roleSkills/180x180/*.png`, context.season, upload)
     }
 
     lastWotUploadKey = uploadKey
 
-    // const res = await clickhouse.query({
-    //   query: `select argMax(gameVersion, dateTime) as version, argMax(season, dateTime) as season from WOT.Event_OnComp7Info where region in ('EU', 'NA')`
-    // })
-    // const data = await res.json<{ version: string, season: string }>()
-    // const { version: latestVersion, season } = data.data[0]
-    // const latestComp7Minor = latestVersion.split('_')[1]
-    // const assetsVersionMinor = assetsVersion.split('.').slice(0, 3).join('.')
-
-    // if (latestComp7Minor != assetsVersionMinor) return
-
-    // const uploadKey = `${season}-${version}`
-    // if (lastWotUploadKey == uploadKey) return console.info(`Comp7 images for season ${season} and version ${version} already uploaded, skip`)
-
-    // await process(`${root}/gui/maps/icons/comp7/ranks/420/*.png`, 'large', season, upload)
-    // await process(`${root}/gui/maps/icons/comp7/ranks/150/*.png`, 'medium', season, upload)
-    // await process(`${root}/gui/maps/icons/comp7/ranks/84/*.png`, 'small', season, upload)
-
-    // lastWotUploadKey = uploadKey
   }
+
 }
